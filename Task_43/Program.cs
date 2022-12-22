01f662b44771ec71fbde61f218d9eb6dd482e579
Timerkhan <timerkhan12@gmail.com>
2022-12-22 21:56:29 +0500

    done

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -11,13 +11,16 @@ void CrossPoint(double b1, double k1, double b2, double k2)
     double yCross = 0;
     if (b1==b2 & k1==k2)
     {
-            System.Console.WriteLine("Прямые совпадают");
+        System.Console.WriteLine("Прямые совпадают");
     } 
-    else if (k1==k2) Console.WriteLine("Прямые параллельны друг другу");
+    else if (k1==k2)
+    {
+         System.Console.WriteLine("Прямые параллельны друг другу");
+    }
     else 
     {
-        xCross =(b2-b1)/(k1-k2);
-        yCross =k1*xCross+b1;
+        xCross = (b2-b1)/(k1-k2);
+        yCross = k1*xCross+b1;
         System.Console.WriteLine("Точка пересечения: ("+ xCross +" ;"+ yCross + ")");
     }
 }
