e0e2a60439b7e05c0386c7935c0b589aca66caec
Timerkhan <timerkhan12@gmail.com>
2022-12-22 21:44:25 +0500

    done

---
+++ Task_41/Program.cs
@@ -0,0 +1,23 @@
+﻿int [] Numbers()
+{
+    Console.Write("Введите числа через пробел: ");
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    return arr;
+}
+
+int Counter(int[] arr)
+{
+    int count = 0; 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine(($"Количествво чисел > 0: {count}"));
+    return count;
+}
+
+int[] arr = Numbers();
+int plus = Counter(arr);
